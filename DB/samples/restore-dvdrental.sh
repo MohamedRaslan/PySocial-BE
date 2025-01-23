92a0a89ccd0718a16c302bbdd033135e07d3b796
MohamedRaslan <mohamedraslang@gmail.com>
2025-01-23 16:18:12 +0200

    feat: Add .gitattributes for line ending normalization and update restore script to use DB_NAME

--- DB/samples/restore-dvdrental.sh
+++ DB/samples/restore-dvdrental.sh
@@ -2,12 +2,12 @@
 set -e
 
 # Wait for PostgreSQL to start
-until pg_isready -U ${DB_USERNAME} -d ${DB_USERNAME}; do
+until pg_isready -U ${DB_USERNAME} -d ${DB_NAME}; do
   sleep 1
 done
 
 
 # Restore the dvdrental database
-pg_restore -U ${DB_USERNAME} -d ${DB_USERNAME} /docker-entrypoint-initdb.d/dvdrental.tar
+pg_restore -U ${DB_USERNAME} -d ${DB_NAME} /docker-entrypoint-initdb.d/dvdrental.tar
 
 echo "dvdrental database restored successfully!"
